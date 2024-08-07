8aea377af418bff9dea78f4082cd75760f5b41b2
Konstantin <krip.1.101@gmail.com>
2024-08-07 21:07:03 +0500

    selection of user by login

--- src/apps/admin/database/DAOs/userDAO.py
+++ src/apps/admin/database/DAOs/userDAO.py
@@ -4,7 +4,7 @@ from src.apps.admin.database.models.rights import Rights
 from src.apps.admin.database.models.group import Group
 from src.apps.admin.security import hasher
 from src.apps.common.database.connection import get_cursor
-from src.apps.common.database.utils import execute_and_fetchall, read_sql_file
+from src.apps.common.database.utils import execute_and_fetchall, read_sql_file, execute_and_fetchone
 
 
 def get_all_users() -> tuple[User]:
@@ -24,7 +24,13 @@ def get_users_by_rights(rights: Rights) -> tuple[User]:
 
 
 def get_user_by_login(login) -> User | None:
-    raise NotImplementedError
+    data = execute_and_fetchone('./SQL/select_user_by_login.sql', (login,))
+
+    if data is None:
+        return None
+
+    group = get_group_by_id(data[3])
+    return User(id=data[0], login=data[1], password=data[2], group=group)
 
 
 def create_user(login: str, password: str, group: Group) -> (User | None, bool):
