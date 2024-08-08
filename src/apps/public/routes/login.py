341023b7618daa2822990e1fe791d9cf0857b8b9
Novikau Arseniy <novikov.arsenij@gmail.com>
2024-08-08 14:15:55 +0300

    create login

--- src/apps/public/routes/login.py
+++ src/apps/public/routes/login.py
@@ -2,13 +2,20 @@
 
 from fastapi import APIRouter, Form
 from typing import Annotated
+from src.apps.admin.database.DAOs.userDAO import get_user_by_login
+from src.apps.admin.security.hasher import hash_password
 
 router = APIRouter()
 
 
 @router.post('/login')
 async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    # check if user exists
-    # check password
+    user = get_user_by_login(username)
 
-    return {"code": 403, "message": "Access denied"}
+    if user is None:
+        return {"code": 404, "message": "User not found"}
+
+    if user.password == hash_password(password, user.salt):
+        return {"code": 200, "message": "Login successful"}
+    else:
+        return {"code": 403, "message": "Access denied"}
