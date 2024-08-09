9932fbd444066000d96629d75a277d18358e0f62
Konstantin <krip.1.101@gmail.com>
2024-08-09 20:21:21 +0500

    endpoints update

--- src/apps/admin/routes/registration.py
+++ src/apps/admin/routes/registration.py
@@ -1,18 +1,21 @@
 from fastapi import APIRouter, Form
 from typing import Annotated
 
+from fastapi.responses import ORJSONResponse
+from starlette import status
+
 from src.apps.admin.database.DAOs.userDAO import create_user
 from src.apps.admin.database.DAOs.groupDAO import get_group_by_name
 
 router = APIRouter()
+router.default_response_class = ORJSONResponse
 
 
-@router.post("/registration")
+@router.post("/registration", status_code=status.HTTP_201_CREATED)
 def register_user(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    print("register user")
     user, created = create_user(username, password, get_group_by_name("default"))
-    print(user)
+
     if created:
-        return {"code": 201, "message": "User registered successfully"}
-    else:
-        return {"code": 500, "message": "Something went wrong"}
+        return ORJSONResponse([{"message": "User created successfully"}])
+
+    return ORJSONResponse([{"message": "User already exists"}])
